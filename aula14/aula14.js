6dae7f33f5890b08a4749eb67283cefc0915388f
Viniciusalvesrj <viniciusalvesrj.dev@gmail.com>
2023-01-19 21:09:56 -0300

    Aula 14

---
+++ aula14/aula14.js
@@ -0,0 +1,31 @@
+//Imprecisão numérica
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+console.log(num1 + num2)
+
+//Para realizar a alteração da variável para um tipo String
+// console.log(num1.toString() + num2);
+
+//para realizar uma alteração permanente
+// num1 = num1.toString();
+// console.log(typeof num1);
+
+//Para gerar uma representação binária
+// console.log(num1.toString(2));
+
+//diminuindo a quantidade de caracteres
+// console.log(num1.toFixed(2));.
+// console.log(Number.isInteger(num1));
+
+// let temp = num1 * '5'; não fazer a tentativa de operações matemáticas com strings
+
+// console.log(Number.isNaN(temp));
+
+
+
+
+
+
+
