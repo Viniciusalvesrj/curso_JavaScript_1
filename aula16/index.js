4d7447e585fb4ae9357632a36d25be7b61c64e21
Viniciusalvesrj <viniciusalvesrj.dev@gmail.com>
2023-01-23 00:15:20 -0300

    Atualizando aula16 e aula17

---
+++ aula16/index.js
@@ -0,0 +1,25 @@
+const alunos = [
+    'Luiz',
+    'Maria',
+    'João'
+];
+
+alunos.push('Luiza'); //Adiciona no fim
+alunos.push('Fábio'); //Adiciona no fim
+
+
+
+ console.log(alunos);
+
+ //  console.log(alunos instanceof Array);
+
+
+// console.log(alunos.slice(0, -3));
+
+// const removido = alunos.shift(); //Remove o primeiro elemento
+// const removido = alunos.pop(); //Remove o último elemento
+
+// alunos.unshift('Vinicius'); //Adiciona no começo do array
+// alunos.unshift('Carolini'); //Adiciona no começo do array
+// alunos.push('Luiza'); //Adiciona no fim
+// alunos.push('Fábio'); //Adiciona no fim
---
+++ aula17/index.js
@@ -0,0 +1,38 @@
+// function saudacao(nome) {
+    
+//     return `Bom dia ${nome}!`;
+
+// };
+
+// const variavel = saudacao('Vinicius');
+// console.log(variavel);
+
+
+// function soma(x = 1, y = 2) {
+//     const resultado = x + y;
+
+//     return resultado;
+// }
+
+// console.log(soma(2, 2));
+
+// console.log(soma(3, 5));
+
+// console.log(soma(20, -2));
+
+// console.log(soma())
+
+// const raiz = function(n) {
+//     return n ** 0.5;
+// };
+
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
